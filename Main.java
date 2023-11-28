bad3707de67d72c63dc62c701ed580a76b6271b5
Samuel044547 <148778928+Samuel044547@users.noreply.github.com>
2023-11-28 17:30:37 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,136 @@
+public class Main {
+    public static void main(String[] args) {
+//Task 1 +
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println();
+//        System.out.println("While цикл ");
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println("Salem Java");
+//            i++;
+//        }
+// Task 2 +
+//int i=0;
+//        while (i<5){
+//            System.out.println(i+" ");
+//           i++;
+//       }
+        // Output 0 1 2 3 4 5
+// Taks 3 +
+//int j = 10;
+//       while (j>5){
+//           j++;
+//           System.out.println(j+" ");
+//        }
+        // Output 5 10 15 ... n
+// Taks 4 +
+//int i=0;
+//      while (true){
+//         i++;
+//           System.out.println(i+" ");
+//            if (i==7){
+//                break;
+//            }}
+// Taks 5 +
+//int j=0;
+//        while (j<8) {
+//            j++;
+//            if (j==5){
+//              continue;
+//            }
+//           System.out.println(j+" ");
+//        }
+// Taks 6
+//int i=0;
+//       do {
+//           i++;
+//          System.out.println(i+"");
+//       }while (i<8);
+//Taks 7 +
+//       int i=10;
+//       do{
+//            i++;
+//            System.out.println(+i+"");
+//       }while (i==8);
+// Taks 8
+//        for (int i=0; i<5; i++);
+//System.out.println();
+// Output: 0 1 2 3 4
+//Taks 9
+//        int i=0;
+// for ( i=0;i<=10;i=i+2);
+// System.out.println("Taks 9 "+i+"");
+// Output 0 2 4 6 8 10
+// Taks 10
+// for(int i=10;i>=0;i--);
+// System.out.println("Taks 10 "+i+" ");
+        // Output 10 9 8 7 6 5 4 3 2 1 0
+        //Taks 11
+
+// for (int i=1;i<=3;i++){
+//     for(i=1;i<=3;j++){
+//         System.out.println("Taks 11 "+i+""+j);
+//} }
+        // Esepter
+// Problem 1
+//        int q=0;
+//       while (true){
+//           q+=10;
+//           System.out.println(q);
+//           if(q>150){
+//               break;
+//           }}
+//
+//       int w=0;
+//    do {
+//        w += 2;
+//          System.out.println(w);
+//    }while (w<40);
+//
+//       int e=0;
+// while (e>90){
+//    e+=5;
+//    System.out.println(e);
+// }
+//
+//       int r=0;
+// do{
+//    r++;
+//    if (r==3) {
+//        continue;
+//    }
+//    System.out.println(r);
+// }while (r<30);
+// Problem 2
+for( int y=10;y<=150;y+=10){
+    if (y==160){
+        break;
+    }
+    System.out.println(y);
+        }
+//
+        for( int y=2;y<=40;y+=2){
+            if (y==42){
+                break;
+            }
+            System.out.println(y);
+        }
+//
+        for( int y=5;y<=90;y+=5){
+            if (y==95){
+                break;
+            }
+            System.out.println(y);
+        }
+//
+        for( int y=0;y<=30;y++){
+            if (y==3){
+                continue;
+            }
+            System.out.println(y);
+        }
+        }}
